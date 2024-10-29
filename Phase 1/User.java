456e628bf29ed68e098cdd6a525f86657d89c844
GT27-eth <124609020+GT27-eth@users.noreply.github.com>
2024-10-29 15:13:21 -0400

    added userList to keep track of every single user

--- Phase 1/User.java
+++ Phase 1/User.java
@@ -3,10 +3,12 @@ class User {
   private String userName;
   // private String displayName // Add if we decide to add after discussion
   private String userPassword;
+  private ArrayList<User> userList;
 
   public User(String username, String password ) {    
     this.userName = username;
     this.userPassword = password;
+    userList.add(this);
   }
 }
   
