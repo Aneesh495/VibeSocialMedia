6e578eb4fc03034315f3b42fb81f8be567a13d41
GT27-eth <124609020+GT27-eth@users.noreply.github.com>
2024-10-29 15:09:52 -0400

    Create User.java and make basic fields/constructor for future reference

---
+++ Phase 1/User.java
@@ -0,0 +1,14 @@
+class User {
+  private static int totalUsers;
+  private String userName;
+  // private String displayName // Add if we decide to add after discussion
+  private String userPassword;
+
+  public User(String username, String password ) {    
+    this.userName = username;
+    this.userPassword = password;
+  }
+}
+  
+  
+  
