740e137792ab5c591f7e712b8c8382f224e3b321
GT27-eth <124609020+GT27-eth@users.noreply.github.com>
2024-10-31 14:00:08 -0400

    complete file r/w methods (half done)

--- Phase 1/SocialServer.java
+++ Phase 1/SocialServer.java
@@ -2,22 +2,34 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 public class SocialServer implements Server {
-    private BufferedReader br;
-    private PrintWriter pw;
+    // private BufferedReader br;
+    // private PrintWriter pw;
     private final String Users = "./Database/userPassword.txt";
     private final String UserInfo = "./Database/userInfo.txt";
     private final String FriendList = "./Database/friends.txt";
-    private final String BlockedList = "fileName";
+    private final String BlockedList = "./Database/blocked.txt";
     private final String Reported = "fileName";
 
-    public SocialServer(BufferedReader br, PrintWriter pw) {
-        this.br=br;
-        this.pw=pw;
+    public SocialServer() {
+        // this.br=br;
+        // this.pw=pw;
+        super()
     }
-    public void createUser(String username, String password, String profilePicture) {
+    public void createUser(String username, String password /* String profilePicture*/ ) { 
+        // We should make another overloaded method with pfp
+        try(PrintWriter pw = new PrintWriter(new FileWriter(Users))) {
+            pw.write(username + " | " + password);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     
     }
     public void editUserInfo(User user) {
+        try(PrintWriter pw = new PrintWriter(new FileWriter(UserInfo));
+           BufferedReader bfr = new BufferedReader(new FileReader(UserInfo))) {
+            
+            
     
     }
     public void blockUser(String userId) {
