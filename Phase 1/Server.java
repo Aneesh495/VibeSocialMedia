5b7dc28accc28c6b95d337562225220c4b5c8912
Tasha Nishid <tnishid@purdue.edu>
2024-11-02 02:54:10 -0400

    Update Server.java

--- Phase 1/Server.java
+++ Phase 1/Server.java
@@ -1,6 +1,13 @@
 public interface Server {
-    public void createUser(String username, String password, String profilePicture);
-    public void editUserInfo(User user);
-    public void blockUser(String UserId);
-    public void getMessage(String UserId1);
+    public void createUser(String username, String password, String profilePicture) 
+           throws UsernameNotValidException, PasswordNotValidException, IOException;
+
+    public void editUserInfo(User user) 
+           throws UserNotFoundException, InvalidBioException, IOException;
+
+    public void blockUser(String UserId) 
+           throws UserNotFoundException, UserAlreadyBlockedException;
+
+    public void getMessage(String UserId1) 
+           throws UserNotFoundException, UserBlockedException;
 }
