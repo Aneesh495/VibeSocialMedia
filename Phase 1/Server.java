233f197356599c457d29932f53408cede6a84ee0
GT27-eth <124609020+GT27-eth@users.noreply.github.com>
2024-11-03 22:27:58 -0500

    Update Server.java

--- Phase 1/Server.java
+++ Phase 1/Server.java
@@ -1,16 +1,12 @@
 import Exceptions.ServerException.*;
-import ServerException.InvalidCredentialsException;
 
 import java.io.*;
-public interface ServerInterface {
-       
-       public void createUser(User user) throws InvalidCredentialsException, IOException;
-       
-       public void createUser(String username, String password) throws InvalidCredentialsException, IOException;
-       
-       // public void editUserInfo(User user) throws UserNotFoundException, UserInputException, IOException;
-   
-       // public void blockUser(String userId) throws UserNotFoundException, BlockedActionException;
-   
-       // public void getMessage(String userId1) throws UserNotFoundException, BlockedActionException;
-   }
+public interface Server {
+
+    public void createUser(User user) throws InvalidCredentialsException, IOException;
+
+    public void createUser(String username, String password) throws InvalidCredentialsException, IOException;
+
+    // public void editUserInfo(User user) throws UserNotFoundException, UserInputException, IOException;
+
+}
